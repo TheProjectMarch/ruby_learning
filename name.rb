d3da3104ad4f71444070e12a95cfd36ee042bb05
theprojectmarch <theprojectmarch@gmail.com>
2021-01-02 10:18:41 -0500

    Add exercise 3 from launchschool

--- name.rb
+++ name.rb
@@ -7,3 +7,8 @@ puts "Please enter your name :)"
 name = gets.chomp
 
 puts greeting + name
+
+#Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+10.times do
+  puts name
+end
