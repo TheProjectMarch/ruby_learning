fc0b2350d3b8fccd152986d31f31d599df54e2d4
theprojectmarch <theprojectmarch@gmail.com>
2021-01-02 10:09:25 -0500

    Transfered to named program file to be consistent

---
+++ name.rb
@@ -0,0 +1,9 @@
+#Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
+
+greeting = "Welcome, "
+
+puts "Please enter your name :)"
+
+name = gets.chomp
+
+puts greeting + name
