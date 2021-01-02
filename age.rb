5b82bb90df95ffae8fae1989d876c107338c4605
theprojectmarch <theprojectmarch@gmail.com>
2021-01-02 10:13:47 -0500

    Add age.rb program

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "Hello! May I ask how old you are?"
+user_age = gets
+plus_ten = user_age.to_i + 10
+plus_twenty = user_age.to_i + 20
+plus_thirty = user_age.to_i + 30
+plus_forty = user_age.to_i + 40
+
+puts "Did you know that in 10 years you'll be " + plus_ten.to_s
+puts "Did you know that in 20 years you'll be " + plus_twenty.to_s
+puts "Did you know that in 30 years you'll be " + plus_thirty.to_s
+puts "Did you know that in 40 years you'll be " + plus_forty.to_s
