55109f9e9101541239a14655721ded0e4cf92e0c
theprojectmarch <theprojectmarch@gmail.com>
2021-01-02 11:31:22 -0500

    Fix mistake of renamed string. Fixed error in example from website

--- bastard_book.rb
+++ bastard_book.rb
@@ -1,6 +1,6 @@
-=begin
+#begin
 
-rename the variables in the following chunk of code so that the output is functionally the same 
+#rename the variables in the following chunk of code so that the output is functionally the same 
 
 variable_x = 90 
 carton = "a" 
@@ -8,11 +8,11 @@ xf_var = (carton + variable_x.to_s) + carton
 a = variable_x.to_s.upcase 
 b = "#{a}#{variable_x}" 
 xf_var += (variable_x + xf_var.to_i).to_s
-puts b + carton 
+puts b + carton + xf_var
 variable_x = variable_x.to_s + "variable_x" + b
 puts variable_x
 
-=end
+#end
 
 #renamed stuffs
 var_q = 90 
@@ -21,6 +21,6 @@ fl_v4r = (plonker + var_q.to_s) + plonker
 r = var_q.to_s.upcase 
 t = "#{r}#{var_q}" 
 fl_v4r += (var_q + fl_v4r.to_i).to_s 
-puts t + plonker
-var_q = var_q.to_s + "var_q" + t
+puts t + plonker + fl_v4r
+var_q = var_q.to_s + "variable_x" + t
 puts var_q
